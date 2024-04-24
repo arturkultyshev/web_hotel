0130e13176aa25c7ff60431e6c266427a843d454
AdilWhy <114363664+AdilWhy@users.noreply.github.com>
2024-04-24 15:26:02 +0500

    feat: jwt tokens, views

--- web_hotel_back/api/views/order_views.py
+++ web_hotel_back/api/views/order_views.py
@@ -1,52 +1,53 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from api.serializers import HotelSerializer
+from api.serializers import OrderSerializer
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-from ..models import Hotel
+from ..models import Order
 
-class HotelsView(APIView):
+
+class OrdersView(APIView):
     def get(self, request):
-        hotels = Hotel.objects.all()
-        serializer = HotelSerializer(hotels, many=True)
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @method_decorator(csrf_exempt)
     def post(self, request):
-        serializer = HotelSerializer(data=request.data)
+        serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class HotelDetailView(APIView):
+
+
+class OrderDetailView(APIView):
     def get_object(self, pk=None):
         try:
-            hotel = Hotel.objects.get(pk=pk)
-            return hotel
-        except Hotel.DoesNotExist as err:
+            order = Order.objects.get(pk=pk)
+            return order
+        except Order.DoesNotExist as err:
             return Response({'error': str(err)}, status=status.HTTP_404_NOT_FOUND)
-    
+
     def get(self, request, id):
-        hotel = self.get_object(pk=id)
-        serializer = HotelSerializer(hotel)
+        order = self.get_object(pk=id)
+        serializer = OrderSerializer(order)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     @method_decorator(csrf_exempt)
     def put(self, request, id):
-        hotel = self.get_object(pk=id)
-        serializer = HotelSerializer(hotel, data=request.data)
+        order = self.get_object(pk=id)
+        serializer = OrderSerializer(order, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     @method_decorator(csrf_exempt)
     def delete(self, request, id):
-        hotel = self.get_object(pk=id)
-        hotel.delete()
+        order = self.get_object(pk=id)
+        order.delele()
         return Response({'deleted': True}, status=status.HTTP_204_NO_CONTENT)
-    
--- web_hotel_back/api/views/user_views.py
+++ web_hotel_back/api/views/user_views.py
@@ -1,44 +1,44 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from api.serializers import HotelSerializer
+from api.serializers import UserSerializer
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-from ..models import Hotel
+from ..models import User
 
-class HotelsView(APIView):
+class UsersView(APIView):
     def get(self, request):
-        hotels = Hotel.objects.all()
-        serializer = HotelSerializer(hotels, many=True)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
     @method_decorator(csrf_exempt)
     def post(self, request):
-        serializer = HotelSerializer(data=request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class HotelDetailView(APIView):
+class UserDetailView(APIView):
     def get_object(self, pk=None):
         try:
-            hotel = Hotel.objects.get(pk=pk)
-            return hotel
-        except Hotel.DoesNotExist as err:
+            user = User.objects.get(pk=pk)
+            return user
+        except User.DoesNotExist as err:
             return Response({'error': str(err)}, status=status.HTTP_404_NOT_FOUND)
     
     def get(self, request, id):
-        hotel = self.get_object(pk=id)
-        serializer = HotelSerializer(hotel)
+        user = self.get_object(pk=id)
+        serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     @method_decorator(csrf_exempt)
     def put(self, request, id):
-        hotel = self.get_object(pk=id)
-        serializer = HotelSerializer(hotel, data=request.data)
+        user = self.get_object(pk=id)
+        serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -46,7 +46,6 @@ class HotelDetailView(APIView):
     
     @method_decorator(csrf_exempt)
     def delete(self, request, id):
-        hotel = self.get_object(pk=id)
-        hotel.delete()
+        user = self.get_object(pk=id)
+        user.delete()
         return Response({'deleted': True}, status=status.HTTP_204_NO_CONTENT)
-    
